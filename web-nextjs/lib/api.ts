ca3c7515d4a46c5bdfe416492825fdf08dbb2d90
Franziska Fieke <grenzbotin@googlemail.com>
2021-06-02 20:08:18 +0200

    fix api call

--- web-nextjs/lib/api.ts
+++ web-nextjs/lib/api.ts
@@ -51,20 +51,8 @@ export interface Project {
 // Helper to make GET requests to Strapi
 export const fetchAPI = async (path: string): Promise<Project[]> => {
   const requestUrl = getStrapiURL(path);
+  const response = await fetch(requestUrl);
+  const data = await response.json();
 
-  fetch(requestUrl)
-    .then(response => response.json())
-    .then(data => {
-      return data;
-    })
-    .catch((error) => {
-      console.error('Error:', error);
-      console.log(error);
-
-      const projects = [] as Project[];
-      return projects;
-    });
-
-    const projects = [] as Project[];
-    return projects;
+  return data;
 };
