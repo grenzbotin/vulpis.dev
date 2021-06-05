216a990413b7eb0bd0049643b1ed998b4ac9655e
Franziska Fieke <grenzbotin@googlemail.com>
2021-06-05 20:57:05 +0200

    SVG improvements

--- web-nextjs/next-sitemap.js
+++ web-nextjs/next-sitemap.js
@@ -1,10 +1,10 @@
 module.exports = {
   siteUrl: 'https://vulpis.dev',
   changefreq: 'monthly',
-  priority: 0.8,
+  priority: 0.7,
   generateRobotsTxt: true,
   sitemapSize: 5000,
-  exclude: ['/privacy', '/disclosure'],
+  exclude: ['/privacy', '/disclosure', '/de/privacy', '/en/privacy', '/de/disclosure', '/en/disclosure'],
   alternateRefs: [
     {
       href: 'https://vulpis.dev/de',
