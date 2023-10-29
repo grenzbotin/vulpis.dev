f8870e7380a0431548ba608c27d40da01f87c823
grenzbotin <grenzbotin@googlemail.com>
2023-10-29 21:12:53 +0100

    setup github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import astroI18next from "astro-i18next";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://www.vulpis.dev",
   integrations: [astroI18next(), compress()],
 });
